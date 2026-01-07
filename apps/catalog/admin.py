f3e839f4edcf6bb4b1353b1f3193c9f9bdf4f6e9
STQT <productgsm@gmail.com>
2026-01-07 10:47:18 +0500

    admin panel changes

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,3 +1,297 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from apps.catalog.models import (
+    Carpet,
+    Collection,
+    Color,
+    Gallery,
+    News,
+    Room,
+    Style,
+    TypeCarpetCollection,
+)
+
+
+@admin.register(TypeCarpetCollection)
+class TypeCarpetCollectionAdmin(admin.ModelAdmin):
+    """Админка для типов ковров"""
+    list_display = ["image_preview", "type", "slug", "created_at"]
+    list_display_links = ["type"]
+    search_fields = ["type", "description"]
+    prepopulated_fields = {"slug": ("type",)}
+    readonly_fields = ["image_preview", "created_at"]
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("type", "slug", "description")
+        }),
+        ("Изображение", {
+            "fields": ("image", "image_preview")
+        }),
+    )
+
+    def image_preview(self, obj):
+        """Превью изображения"""
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-width: 100px; max-height: 100px; object-fit: cover; border-radius: 4px;"/>',
+                obj.image.url
+            )
+        return "-"
+    image_preview.short_description = "Превью"
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    """Админка для коллекций"""
+    list_display = ["image_preview", "name", "type", "is_published", "is_new", "carpets_count", "created_at"]
+    list_display_links = ["name"]
+    list_filter = ["is_published", "is_new", "type", "created_at"]
+    search_fields = ["name", "description"]
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ["image_preview", "created_at", "update_at", "carpets_count"]
+    date_hierarchy = "created_at"
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("name", "slug", "type", "description")
+        }),
+        ("Изображение", {
+            "fields": ("image", "image_preview")
+        }),
+        ("Настройки", {
+            "fields": ("is_published", "is_new")
+        }),
+        ("Даты", {
+            "fields": ("created_at", "update_at")
+        }),
+    )
+
+    def image_preview(self, obj):
+        """Превью изображения"""
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-width: 100px; max-height: 100px; object-fit: cover; border-radius: 4px;"/>',
+                obj.image.url
+            )
+        return "-"
+    image_preview.short_description = "Превью"
+
+    def carpets_count(self, obj):
+        """Количество ковров в коллекции"""
+        return obj.carpets.count()
+    carpets_count.short_description = "Количество ковров"
+
+
+@admin.register(Style)
+class StyleAdmin(admin.ModelAdmin):
+    """Админка для стилей"""
+    list_display = ["name", "slug", "carpets_count"]
+    list_display_links = ["name"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ["carpets_count"]
+
+    def carpets_count(self, obj):
+        """Количество ковров со стилем"""
+        return obj.carpets.count()
+    carpets_count.short_description = "Количество ковров"
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    """Админка для комнат"""
+    list_display = ["name", "slug", "carpets_count"]
+    list_display_links = ["name"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ["carpets_count"]
+
+    def carpets_count(self, obj):
+        """Количество ковров для комнаты"""
+        return obj.carpets.count()
+    carpets_count.short_description = "Количество ковров"
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    """Админка для цветов"""
+    list_display = ["color_preview", "name", "slug", "hex_code", "carpets_count"]
+    list_display_links = ["name"]
+    search_fields = ["name"]
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ["color_preview", "carpets_count"]
+
+    def color_preview(self, obj):
+        """Превью цвета"""
+        if obj.hex_code:
+            return format_html(
+                '<div style="width: 30px; height: 30px; background-color: {}; border-radius: 4px; border: 1px solid #ddd;"></div>',
+                obj.hex_code
+            )
+        return "-"
+    color_preview.short_description = "Цвет"
+
+    def carpets_count(self, obj):
+        """Количество ковров с цветом"""
+        return obj.carpets.count()
+    carpets_count.short_description = "Количество ковров"
+
+
+class CarpetImageInline(admin.TabularInline):
+    """Inline для дополнительных изображений ковра (если понадобится в будущем)"""
+    model = Carpet
+    extra = 0
+    fields = ["photo", "code"]
+    readonly_fields = ["photo"]
+
+
+@admin.register(Carpet)
+class CarpetAdmin(admin.ModelAdmin):
+    """Админка для ковров"""
+    list_display = [
+        "photo_preview",
+        "code",
+        "collection",
+        "material",
+        "is_new",
+        "is_popular",
+        "is_published",
+        "watched",
+        "created_at"
+    ]
+    list_display_links = ["code"]
+    list_filter = [
+        "is_published",
+        "is_new",
+        "is_popular",
+        "collection",
+        "styles",
+        "rooms",
+        "colors",
+        "roll",
+        "created_at"
+    ]
+    search_fields = ["code", "material", "collection__name"]
+    readonly_fields = [
+        "photo_preview",
+        "watched",
+        "created_at",
+        "update_at"
+    ]
+    filter_horizontal = ["styles", "rooms", "colors"]
+    date_hierarchy = "created_at"
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("code", "collection", "roll")
+        }),
+        ("Изображение", {
+            "fields": ("photo", "photo_preview")
+        }),
+        ("Характеристики", {
+            "fields": (
+                "material",
+                "density",
+                "base",
+                "pile_height",
+                "yarn_composition",
+                "weight"
+            )
+        }),
+        ("Фильтры", {
+            "fields": ("styles", "rooms", "colors", "is_new", "is_popular")
+        }),
+        ("Статистика", {
+            "fields": ("watched",)
+        }),
+        ("Настройки", {
+            "fields": ("is_published",)
+        }),
+        ("Даты", {
+            "fields": ("created_at", "update_at")
+        }),
+    )
+
+    def photo_preview(self, obj):
+        """Превью изображения ковра"""
+        if obj.photo:
+            return format_html(
+                '<img src="{}" style="max-width: 150px; max-height: 150px; object-fit: cover; border-radius: 4px;"/>',
+                obj.photo.url
+            )
+        return "-"
+    photo_preview.short_description = "Превью"
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    """Админка для новостей"""
+    list_display = ["image_preview", "title", "is_published", "created_at"]
+    list_display_links = ["title"]
+    list_filter = ["is_published", "created_at"]
+    search_fields = ["title", "description", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ["image_preview", "created_at", "update_at"]
+    date_hierarchy = "created_at"
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("title", "slug", "description", "content")
+        }),
+        ("Изображение", {
+            "fields": ("image", "image_preview")
+        }),
+        ("Настройки", {
+            "fields": ("is_published",)
+        }),
+        ("Даты", {
+            "fields": ("created_at", "update_at")
+        }),
+    )
+
+    def image_preview(self, obj):
+        """Превью изображения"""
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-width: 150px; max-height: 150px; object-fit: cover; border-radius: 4px;"/>',
+                obj.image.url
+            )
+        return "-"
+    image_preview.short_description = "Превью"
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    """Админка для галереи"""
+    list_display = ["image_preview", "title", "order", "is_published", "created_at"]
+    list_display_links = ["title"]
+    list_filter = ["is_published", "created_at"]
+    search_fields = ["title", "description"]
+    readonly_fields = ["image_preview", "created_at"]
+    list_editable = ["order", "is_published"]
+    fieldsets = (
+        ("Основная информация", {
+            "fields": ("title", "description", "order")
+        }),
+        ("Изображение", {
+            "fields": ("image", "image_preview")
+        }),
+        ("Настройки", {
+            "fields": ("is_published",)
+        }),
+        ("Даты", {
+            "fields": ("created_at",)
+        }),
+    )
+
+    def image_preview(self, obj):
+        """Превью изображения"""
+        if obj.image:
+            return format_html(
+                '<img src="{}" style="max-width: 200px; max-height: 200px; object-fit: cover; border-radius: 4px;"/>',
+                obj.image.url
+            )
+        return "-"
+    image_preview.short_description = "Превью"
+
+    class Meta:
+        ordering = ["order", "-created_at"]
--- config/settings/base.py
+++ config/settings/base.py
@@ -68,10 +68,11 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # "django.contrib.humanize", # Handy template tags
-    "django.contrib.admin",
     "django.forms",
 ]
 THIRD_PARTY_APPS = [
+    "jazzmin",  # Modern admin interface - must be before django.contrib.admin
+    "django.contrib.admin",
     "crispy_forms",
     "crispy_bootstrap5",
     "allauth",
@@ -315,5 +316,162 @@ SPECTACULAR_SETTINGS = {
     "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
     "SCHEMA_PATH_PREFIX": "/api/",
 }
+# django-jazzmin
+# ------------------------------------------------------------------------------
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "YEC Gilam Admin",
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "YEC Gilam",
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "YEC Gilam",
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    "site_logo": None,
+    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    "login_logo": None,
+    # Logo to use for login form in dark themes (defaults to login_logo)
+    "login_logo_dark": None,
+    # CSS classes that are applied to the logo above
+    "site_logo_classes": "img-circle",
+    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+    "site_icon": None,
+    # Welcome text on the login screen
+    "welcome_sign": "Добро пожаловать в админ-панель YEC Gilam",
+    # Copyright on the footer
+    "copyright": "YEC Gilam",
+    # The model admin to search from the search bar, search bar omitted if excluded
+    "search_model": ["catalog.Carpet", "catalog.Collection", "catalog.News"],
+    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
+    "user_avatar": None,
+    ############
+    # Top Menu #
+    ############
+    # Links to put along the top menu
+    "topmenu_links": [
+        # Url that gets reversed (Permissions can be added)
+        {"name": "Главная", "url": "admin:index", "permissions": ["auth.view_user"]},
+        # external url that opens in a new window (Permissions can be added)
+        {"name": "API Docs", "url": "/api/docs/", "new_window": True},
+        # model admin to link to (Permissions checked against model)
+        {"model": "catalog.Carpet"},
+        # App with dropdown menu to all its models pages (Permissions checked against models)
+        {"app": "catalog"},
+    ],
+    #############
+    # User Menu #
+    #############
+    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
+    "usermenu_links": [
+        {"name": "API Documentation", "url": "/api/docs/", "new_window": True},
+        {"model": "auth.user"},
+    ],
+    #############
+    # Side Menu #
+    #############
+    # Whether to display the side menu
+    "show_sidebar": True,
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+    # Hide these apps when generating side menu e.g (auth)
+    "hide_apps": [],
+    # Hide these models when generating side menu (e.g auth.user)
+    "hide_models": [],
+    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
+    "order_with_respect_to": ["catalog", "auth"],
+    # Custom links to append to app groups, keyed on app name
+    "custom_links": {
+        "catalog": [
+            {
+                "name": "Статистика",
+                "url": "admin:catalog_carpet_changelist",
+                "icon": "fas fa-chart-bar",
+            }
+        ]
+    },
+    # Custom icons for side menu apps/models
+    # See https://fontawesome.com/icons for available icons
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "catalog.TypeCarpetCollection": "fas fa-tags",
+        "catalog.Collection": "fas fa-layer-group",
+        "catalog.Carpet": "fas fa-couch",
+        "catalog.Style": "fas fa-palette",
+        "catalog.Room": "fas fa-home",
+        "catalog.Color": "fas fa-fill-drip",
+        "catalog.News": "fas fa-newspaper",
+        "catalog.Gallery": "fas fa-images",
+    },
+    # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": True,
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    "custom_css": None,
+    "custom_js": None,
+    # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
+    "use_google_fonts_cdn": True,
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False,
+    ###############
+    # Change view #
+    ###############
+    # Render out the change view as a single form, or in tabs, current options are
+    # - single
+    # - horizontal_tabs (default)
+    # - vertical_tabs
+    # - collapsible
+    # - carousel
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {
+        "auth.user": "collapsible",
+        "auth.group": "vertical_tabs",
+    },
+    # Add a language dropdown into the admin
+    "language_chooser": False,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-primary",
+    "accent": "accent-primary",
+    "navbar": "navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    },
+    "actions_sticky_top": False
+}
+
 # Your stuff...
 # ------------------------------------------------------------------------------
